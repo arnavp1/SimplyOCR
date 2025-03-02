58e2d74534f29c65de0b637a28a68f0f98921a06
Arnav Podichetty <arnav.p2024@gmail.com>
2025-03-02 03:25:14 -0800

    Initial Commit

---
+++ SimplyOCR/SimplyOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyOCRApp.swift
+//  SimplyOCR
+//
+//  Created by Arnav Podichetty on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SimplyOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimplyOCRTests/SimplyOCRTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyOCRTests.swift
+//  SimplyOCRTests
+//
+//  Created by Arnav Podichetty on 3/2/25.
+//
+
+import Testing
+@testable import SimplyOCR
+
+struct SimplyOCRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
