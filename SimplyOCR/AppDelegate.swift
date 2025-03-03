7a3822c2411772a8d3fee26740d1229f5d78c799
Arnav Podichetty <arnav.p2024@gmail.com>
2025-03-02 16:14:15 -0800

    Dock and Menu Bar Toggles

--- SimplyOCR/AppDelegate.swift
+++ SimplyOCR/AppDelegate.swift
@@ -9,6 +9,10 @@ import Cocoa
 import SwiftUI
 import KeyboardShortcuts
 
+extension Notification.Name {
+    static let settingsChanged = Notification.Name("settingsChanged")
+}
+
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem?
     
@@ -21,26 +25,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }()
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.regular)
+        // Set activation based on stored setting (default regular)
+        updateAppearance()
         
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        if let button = statusItem?.button {
-            button.title = "SimplyOCR"
-        }
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Settings", action: #selector(openSettings), keyEquivalent: ","))
-        menu.addItem(.separator())
-        menu.addItem(NSMenuItem(title: "Quit SimplyOCR", action: #selector(quitApp), keyEquivalent: "q"))
-        statusItem?.menu = menu
+        NotificationCenter.default.addObserver(self, selector: #selector(settingsDidChange), name: .settingsChanged, object: nil)
         
         if KeyboardShortcuts.getShortcut(for: KeyboardShortcuts.Name.captureAndOCR) == nil {
             KeyboardShortcuts.setShortcut(.init(.s, modifiers: [.command, .shift]), for: KeyboardShortcuts.Name.captureAndOCR)
         }
-
+        
         KeyboardShortcuts.onKeyDown(for: .captureAndOCR) {
             GlobalHotKeyManager.shared.captureAndOCR()
         }
-        
     }
     
     @objc func openSettings() {
@@ -50,5 +46,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @objc func quitApp() {
         NSApp.terminate(nil)
+    }
+    
+    @objc func settingsDidChange() {
+        updateAppearance()
+    }
+    
+    func updateAppearance() {
+        let defaults = UserDefaults.standard
+        let showInDock = defaults.object(forKey: "showInDock") as? Bool ?? true
+        let showInMenuBar = defaults.object(forKey: "showInMenuBar") as? Bool ?? true
+        
+        if showInDock {
+            NSApp.setActivationPolicy(.regular)
+        } else {
+            NSApp.setActivationPolicy(.accessory)
+        }
+        
+        if showInMenuBar {
+            if statusItem == nil {
+                statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+                if let button = statusItem?.button {
+                    button.title = "SimplyOCR"
+                }
+                let menu = NSMenu()
+                menu.addItem(NSMenuItem(title: "Settings", action: #selector(openSettings), keyEquivalent: ","))
+                menu.addItem(.separator())
+                menu.addItem(NSMenuItem(title: "Quit SimplyOCR", action: #selector(quitApp), keyEquivalent: "q"))
+                statusItem?.menu = menu
+            }
+        } else {
+            statusItem = nil
+        }
     }
 }
--- SimplyOCR/SettingsView.swift
+++ SimplyOCR/SettingsView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 import KeyboardShortcuts
 
 struct SettingsView: View {
+    @AppStorage("showInDock") var showInDock: Bool = true
+    @AppStorage("showInMenuBar") var showInMenuBar: Bool = true
+    
     var body: some View {
         Form {
             HStack {
                 Text("OCR Shortcut:")
                 KeyboardShortcuts.Recorder(for: .captureAndOCR)
             }
+            Toggle("Show in Dock", isOn: $showInDock)
+                .onChange(of: showInDock) { _, _ in
+                    NotificationCenter.default.post(name: .settingsChanged, object: nil)
+                }
+            Toggle("Show in Menu Bar", isOn: $showInMenuBar)
+                .onChange(of: showInMenuBar) { _, _ in
+                    NotificationCenter.default.post(name: .settingsChanged, object: nil)
+                }
         }
         .padding()
-        .frame(width: 300, height: 100)
+        .frame(width: 300, height: 150)
     }
 }
